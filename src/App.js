08350cd1349efa4b1e209d7cb89b8c6759d29346
itaytay <itayfry@gmail.com>
2022-03-04 00:37:07 +0200

    step 01 - hardcoded table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Tic Tac Toe
+        </h1>
+        <table>
+          <tr>
+            <td>1</td><td>1</td><td>1</td>
+          </tr>
+          <tr>
+            <td>1</td><td>1</td><td>1</td>
+          </tr>
+          <tr>
+            <td>1</td><td>1</td><td>1</td>
+          </tr>
+        </table>
       </header>
     </div>
   );
