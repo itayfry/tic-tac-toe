dd62dd479b87f82c998172082ed80f8023ec7dee
itaytay <itayfry@gmail.com>
2022-03-04 01:18:24 +0200

    step-03 - props drilling currentPlayer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import { useState } from "react";
 import "./App.css";
 
-const Cell = () => {
-  const [val, setVal] = useState(0)
-  return (<td onClick={() => {setVal(val + 1)}}>{val}</td>);
+const Cell = ({ currentPlayer, toggleCurrentPlayer }) => {
+  const [val, setVal] = useState(null);
+  return (
+    <td
+      onClick={() => {
+        setVal(currentPlayer);
+        toggleCurrentPlayer();
+      }}
+    >
+      {val}
+    </td>
+  );
 };
 
-const Row = () => <tr><Cell/><Cell/><Cell/></tr>
+const Row = ({ currentPlayer, toggleCurrentPlayer }) => (
+  <tr>
+    <Cell currentPlayer={currentPlayer} toggleCurrentPlayer={toggleCurrentPlayer} />
+    <Cell currentPlayer={currentPlayer} toggleCurrentPlayer={toggleCurrentPlayer} />
+    <Cell currentPlayer={currentPlayer} toggleCurrentPlayer={toggleCurrentPlayer} />
+  </tr>
+);
 
 function App() {
+  const [currentPlayer, setCurrentPlayer] = useState("X"); // X Y
+  const toggleCurrentPlayer = () => currentPlayer === 'X' ? setCurrentPlayer('Y') : setCurrentPlayer('X');
   return (
     <div className="App">
       <header className="App-header">
         <h1>Tic Tac Toe</h1>
         <table>
-          <Row/>
-          <Row/>
-          <Row/>
+          <Row
+            currentPlayer={currentPlayer}
+            toggleCurrentPlayer={toggleCurrentPlayer}
+          />
+          <Row
+            currentPlayer={currentPlayer}
+            toggleCurrentPlayer={toggleCurrentPlayer}
+          />
+          <Row
+            currentPlayer={currentPlayer}
+            toggleCurrentPlayer={toggleCurrentPlayer}
+          />
         </table>
       </header>
     </div>
